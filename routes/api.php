69aae280b921cf4fb799ca72004b7a372e7b4f4a
MohamadReza Mokhtari <mr.mokhtari97@gmail.com>
2024-08-21 19:40:00 +0330

    ✨ feat(routes): add task routes with public and secured endpoints.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,16 @@ Route::prefix('auth')->group(function () {
     Route::post('login', LoginController::class)->name('login');
 
 });
+
+Route::prefix('tasks')->name('tasks.')->group(function () {
+    Route::get('/', [TaskController::class, 'index'])->name('index');
+    Route::get('{id}', [TaskController::class, 'show'])->name('show');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('user', [TaskController::class, 'getUserTasks'])->name('userTasks');
+        Route::post('/', [TaskController::class, 'store'])->name('store');
+        Route::put('{id}', [TaskController::class, 'update'])->name('update');
+        Route::delete('{id}', [TaskController::class, 'destroy'])->name('destroy');
+    });
+});
+
