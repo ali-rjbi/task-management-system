5555ac60d5fd879880bc6119963e85aa252e2c6d
MohamadReza Mokhtari <mr.mokhtari97@gmail.com>
2024-08-21 18:30:00 +0330

    ✨ feat(api): add auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('auth')->group(function () {
+
+    Route::post('register', RegisterController::class)->name('register');
+
+    Route::post('login', LoginController::class)->name('login');
+
+});
