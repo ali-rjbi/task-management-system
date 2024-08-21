9e6b3b759a523036a068e921b1ee9affaf28834d
MohamadReza Mokhtari <mr.mokhtari97@gmail.com>
2024-08-21 22:18:00 +0330

    ✨ feat: add real-time updates and task detail modal actions

--- app/Livewire/TasksList.php
+++ app/Livewire/TasksList.php
@@ -4,14 +4,15 @@ namespace App\Livewire;
 
 use App\Models\Task;
 use App\Services\TaskService;
-use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Log;
+use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithPagination;
 
 class TasksList extends Component
 {
     use WithPagination;
+
     public $showNewTaskNotification = false;
 
     protected $taskService;
@@ -29,21 +30,49 @@ class TasksList extends Component
 
     public function render()
     {
-        $tasks = $this->taskService->getTasks([],15);
-        return view('livewire.tasks-list',compact('tasks'));
+        $tasks = $this->taskService->getTasks([], 20);
+        return view('livewire.tasks-list', compact('tasks'));
+    }
+
+    public function showTask($id)
+    {
+        $this->dispatch('show-task-modal', $id);
+    }
+
+    #[On('task-deleted')]
+    #[On('task-updated')]
+    public function refreshTasksList()
+    {
+        $this->resetPage();
+    }
+
+    public function getRealTimeTask(Task $task)
+    {
+        Log::debug('notifyNewTask', [
+            'task' => $task
+        ]);
+    }
+
+    public function getRealTimeTaskDeleted($taskId)
+    {
+        Log::debug('notifyDeletedTask', [
+            'taskId' => $taskId
+        ]);
+    }
+
+    public function getRealTimeTaskUpdated($taskId)
+    {
+        Log::debug('notifyUpdatedTask', [
+            'taskId' => $taskId
+        ]);
     }
 
     protected function getListeners()
     {
         return [
             'echo:tasks,TaskCreated' => 'getRealTimeTask',
+            'echo:tasks,TaskDeleted' => 'getRealTimeTaskDeleted',
+            'echo:tasks,TaskUpdated' => 'getRealTimeTaskUpdated',
         ];
-    }
-
-    public function getRealTimeTask(Task $task)
-    {
-        Log::debug('notifyNewTask',[
-            'task' => $task
-        ]);
     }
 }
