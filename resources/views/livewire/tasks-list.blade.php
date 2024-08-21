209dab8f0a6db1e86d898803f8a5d42cefb154ef
MohamadReza Mokhtari <mr.mokhtari97@gmail.com>
2024-08-21 22:18:00 +0330

    ✨ feat(ui): add action buttons to task cards for showing task details

--- resources/views/livewire/tasks-list.blade.php
+++ resources/views/livewire/tasks-list.blade.php
@@ -10,20 +10,30 @@
                             </div>
                             <div class="flex">
                                 @if($task->due_date)
-                                    <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-white text-gray-800 me-1">
+                                    <span
+                                        class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-white text-gray-800 me-1">
                                     {{ verta($task->due_date)->format('Y/m/d') }}
                                 </span>
                                 @endif
-                                <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800 me-1">
+                                <span
+                                    class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800 me-1">
                                     {{ $task->status->name }}
                                 </span>
-                                <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-blue-100 text-blue-800 me-1">
+                                <span
+                                    class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-blue-100 text-blue-800 me-1">
                                     {{ $task->priority->name }}
                                 </span>
+                                <button
+                                    class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-blue-100 text-blue-800 me-1 min-h-[26.5px]"
+                                    wire:click="showTask({{ $task->id }})"
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400"
+                                         viewBox="0 0 20 20" fill="currentColor">
+                                        <path
+                                            d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"/>
+                                    </svg>
+                                </button>
                             </div>
-                        </div>
-                        <div class="flex pt-1 text-gray-600 dark:text-gray-400">
-                            {{ $task->description }}
                         </div>
                     </div>
                 </div>
@@ -33,5 +43,7 @@
     <div class="mt-4 px-6">
         {{ $tasks->links() }}
     </div>
+
+    <livewire:tasks-modal></livewire:tasks-modal>
 
 </section>
